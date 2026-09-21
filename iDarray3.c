59a8c92c9689f60a4840c7cc1396629baad436bf
akhandpratap25 <akhandgaur25@gmail.com>
2026-09-21 12:04:13 +0530

    Add files via upload

---
+++ iDarray3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int size = 5;
+    int index = 2; 
+    for (int i = index; i < size - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    size--;  
+    for (int i = 0; i < size; i++) printf("%d ", arr[i]);
+    
+    return 0;
+}
